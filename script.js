c09f26d063994ac6c9ba6c0c6b7b7e98c23e5261
AayushiParashar <aayushiparashar02@gmail.com>
2026-06-01 15:02:32 +0530

    created the images folder (#2)

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-'use strict';
+"use strict";
 
 // Node variable declaration
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
 
-const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 // Starting Condition
 let scores, currentScore, activePlayer, playing, winningScore;
@@ -26,14 +26,14 @@ const init = function () {
 
   score0El.textContent = 0;
   score1El.textContent = 0;
-  diceEl.classList.add('hidden');
+  diceEl.classList.add("hidden");
   current0El.textContent = 0;
   current1El.textContent = 0;
 
-  player0El.classList.remove('player--winner');
-  player1El.classList.remove('player--winner');
-  player0El.classList.add('player--active');
-  player1El.classList.remove('player--active');
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
 };
 
 init();
@@ -45,19 +45,19 @@ const switchPlayer = function () {
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   // Color shade change
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 // Rolling dice functionality
 
-btnRoll.addEventListener('click', function () {
+btnRoll.addEventListener("click", function () {
   if (playing) {
     // 1.Genrating a random number dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
     // 2.Display dice
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+    diceEl.classList.remove("hidden");
+    diceEl.src = `images/dice-${dice}.png`;
     // 3.Check for rolled 1:
     if (dice !== 1) {
       currentScore = currentScore + dice; // currentScore += dice;
@@ -71,9 +71,9 @@ btnRoll.addEventListener('click', function () {
 });
 
 // Hold Button Functionality
-btnHold.addEventListener('click', function () {
+btnHold.addEventListener("click", function () {
   if (playing) {
-    diceEl.classList.add('hidden');
+    diceEl.classList.add("hidden");
     // Add current score to the total score
     scores[activePlayer] = scores[activePlayer] + currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
@@ -84,10 +84,10 @@ btnHold.addEventListener('click', function () {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner');
+        .classList.add("player--winner");
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active');
+        .classList.remove("player--active");
     } else {
       // Switch to next player
       switchPlayer();
@@ -96,13 +96,13 @@ btnHold.addEventListener('click', function () {
 });
 
 // New game button
-btnNew.addEventListener('click', init);
+btnNew.addEventListener("click", init);
 
 // Enter player's name
 
-document.querySelector('#name--0').addEventListener('dblclick', function (e) {
-  e.target.textContent = prompt('Enter Your Name');
+document.querySelector("#name--0").addEventListener("dblclick", function (e) {
+  e.target.textContent = prompt("Enter Your Name");
 });
-document.querySelector('#name--1').addEventListener('dblclick', function (e) {
-  e.target.textContent = prompt('Enter Your Name');
+document.querySelector("#name--1").addEventListener("dblclick", function (e) {
+  e.target.textContent = prompt("Enter Your Name");
 });
